f5eda3f5dd83cdbaf4ea12a212bdf7c22e48b239
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-26 00:06:07 +0700

    Second Commit'

---
+++ 19.js
@@ -0,0 +1,10 @@
+//Function
+
+//membuat fungsi luas lingkaran
+luasLingkaran = (r) => {
+    //menggunakan rumus luas lingkaran phi kali r pangkat 2
+    return 3.14 * (r ** 2)
+}
+
+//menampilkan hasil dari penggunaan fungsi luas lingkaran
+console.log("Luas lingkaran dengan r = 10 adalah " + luasLingkaran(10));
